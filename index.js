388e1ea5f191d8d6815c898df73a6a378b7fc352
Josh Massa <joshmassa@gmail.com>
2024-05-26 20:36:41 -0400

    Second commit

--- index.js
+++ index.js
@@ -3,32 +3,99 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
+import { MongoClient, ObjectId } from 'mongodb';
+import { availableParallelism } from 'node:os';
+import cluster from 'node:cluster';
+import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const dbName = 'chatApp';
+let db;
+let messagesCollection;
 
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
-});
+async function main() {
+    await client.connect();
+    console.log('Connected successfully to MongoDB server');
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
+    db = client.db(dbName);
+    messagesCollection = db.collection('messages');
+
+    await messagesCollection.createIndex({ client_offset: 1 }, { unique: true });
+}
+
+main().catch(console.error);
+
+if (cluster.isPrimary) {
+    const numCPUs = availableParallelism();
+    // create one worker per available core
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork({
+        PORT: 3000 + i
+      });
+    }
+    
+    // set up the adapter on the primary thread
+    setupPrimary();
+} else {
+    
+    const app = express();
+    const server = createServer(app);
+    const io = new Server(server, {
+        connectionStateRecovery: {},
+        adapter: createAdapter()
+    });
+    
+    
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    
+    app.get('/', (req, res) => {
+        res.sendFile(join(__dirname, 'index.html'));
+    });
+    
+    io.on('connection', async (socket) => {
+        console.log('a user connected');
+        socket.on('disconnect', () => {
+          console.log('user disconnected');
+        });
+    
+        socket.on('chat message', async (msg, clientOffset, callback) => {
+            console.log('message: ' + msg);
+            let result;
+            try {
+                result = await messagesCollection.insertOne({ content: msg, client_offset: clientOffset });
+            } catch (e) {
+                if (e.code === 11000) {
+                    callback();
+                } else {
+                    console.error('Error inserting message', e);
+                }
+                return;
+            }
+            io.emit('chat message', msg, result.insertedId);
+            callback();
+        });
+    
+        if (!socket.recovered) {
+            try {
+                const serverOffset = socket.handshake.auth.serverOffset || '000000000000000000000000';
+                const messages = await messagesCollection.find({ _id: { $gt: new ObjectId(serverOffset) } }).toArray();
+                messages.forEach((message) => {
+                    socket.emit('chat message', message.content, message._id);
+                });
+            } catch (e) {
+                console.error('Error retrieving messages', e);
+            }
+        }
+    
+        // socket.on('chat message', (msg) => {
+        //     io.emit('chat message', msg);
+        // });
+    });
+    
+    server.listen(3000, () => {
+        console.log('server running at http://localhost:3000');
     });
 
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-    });
-
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
-});
-
-server.listen(3000, () => {
-    console.log('server running at http://localhost:3000');
-});
+}
