b2734e0a6ad9ee080490a38d799edfaebce4e13a
The Other Promise <150410378+noe-bee@users.noreply.github.com>
2024-06-03 09:27:48 -0700

    made the theme switch work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { SocketProvider } from './context/SocketContext';
 import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
-import { Layout } from 'antd';
+import { Layout, Switch, ConfigProvider, theme } from 'antd';
 import LoggedInIndicator from './components/LoggedInIndicator'; // Add this import
-
+// import { ThemeProvider } from './context/ThemeContext';
+// import ThemeSwitcher from './components/ThemeSwitcher';
 
 const client = new ApolloClient({
   uri: 'http://localhost:3000/graphql',
@@ -16,24 +17,62 @@ const client = new ApolloClient({
 const { Sider, Content } = Layout;
 
 const App = () => {
+  const [currentTheme, setCurrentTheme] = useState('light');
+  const changeTheme = (value) => {
+    setCurrentTheme(value ? 'dark' : 'light');
+  };
+  const LightTheme = 
+  {
+    colorBgContainer: '#ffffff', 
+    colorTextBase: '#4b4b4b',
+    "headerBg": "rgb(255, 255, 255)"
+  }
+  const DarkTheme =
+  {
+    colorBgContainer: '#414172', 
+    colorTextBase: '#aeaeae',
+    "bodyBg": "rgb(0, 21, 41)",
+    "headerBg": "rgb(65, 65, 114)"
+  }
+
   console.log('App component rendered');
   return (
     <ApolloProvider client={client}>
       <SocketProvider>
-        <Layout style={{ minHeight: '100vh'}}>
-          <Header />
+        <ConfigProvider
+        theme={{
+      token: 
+        currentTheme === 'light'? LightTheme : DarkTheme,
+      components:{
+        "Layout":
+          currentTheme ==='light'? LightTheme : DarkTheme,        
+      }
+    }}>
+        <Layout 
+
+        style={{ minHeight: '100vh'}}>
+          <Header/>
           <Layout>
             <Sider>
-              <Navigation />
+              <Navigation/>
             </Sider>
             <Layout>
-              <Content style={{ minHeight: '91vh' }}>
+              <Content
+
+              style={{ minHeight: '91vh' }}>
+                <Switch 
+                checked = {currentTheme === 'dark'}
+                checkedChildren = "Dark"
+                unCheckedChildren = "Light"
+                onChange= {changeTheme}
+                />
                 <Outlet />
                 <LoggedInIndicator />
               </Content>
             </Layout>
           </Layout>
         </Layout>
+        </ConfigProvider>
       </SocketProvider>
     </ApolloProvider>
   );
